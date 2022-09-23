f05a85c2239c9e2d694617aa18d2375ca6148270
engks4619 <engks4619@naver.com>
2022-09-23 10:38:38 +0900

    feat: 메인페이지 캐러셀 및 상단메뉴 반응형 작업

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from "react";
+import CarouselComponent from "./CarouselComponent";
+import style from "@/styles/Home.module.scss"
 
 const Home = () => {
-    return(
-        <>
-        </>
-    );
-}
+  const carouselContents = ["/assets/monitor.png", "/assets/keyboard.jpg"];
+
+  return (
+    <>
+      <div className={style.carousel}>
+        <CarouselComponent contents={carouselContents} />
+      </div>
+      <div>
+        hello
+      </div>
+    </>
+  );
+};
 
 export default Home;
