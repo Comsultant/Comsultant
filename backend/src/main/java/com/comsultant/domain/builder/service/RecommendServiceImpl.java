e39efbbaeb98999c77357f3f2d92f9a412941756
JuKyYoon <yoongh97@naver.com>
2022-10-04 20:49:28 +0900

    feat: 추천 API 수정
    - 제품 idx와 제품 이름을 합쳐서 응답
    - 날짜와 가격은 가공 안 한 상태로 전달

--- backend/src/main/java/com/comsultant/domain/builder/service/RecommendServiceImpl.java
+++ backend/src/main/java/com/comsultant/domain/builder/service/RecommendServiceImpl.java
@@ -2,7 +2,11 @@ package com.comsultant.domain.builder.service;
 
 import com.comsultant.domain.builder.dto.RecommendBuilderDto;
 import com.comsultant.domain.builder.dto.RecommendBuilderResponseDto;
+import com.comsultant.domain.product.entity.*;
+import com.comsultant.domain.product.repository.*;
 import com.comsultant.global.config.security.AccountDetails;
+import com.comsultant.global.error.exception.CommentApiException;
+import com.comsultant.global.error.model.CommentErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Sort;
@@ -23,6 +27,16 @@ public class RecommendServiceImpl implements RecommendService {
 
     private final MongoTemplate mongoTemplate;
 
+    private final CpuRepository cpuRepository;
+    private final RamRepository ramRepository;
+    private final HddRepository hddRepository;
+    private final SsdRepository ssdRepository;
+    private final PsuRepository psuRepository;
+    private final CoolerRepository coolerRepository;
+    private final CasesRepository casesRepository;
+    private final MainBoardRepository mainBoardRepository;
+    private final VgaRepository vgaRepository;
+
     @Override
     public List<RecommendBuilderResponseDto> getRecommendBuilder(AccountDetails accountDetails, RecommendBuilderDto dto) {
 
@@ -40,11 +54,15 @@ public class RecommendServiceImpl implements RecommendService {
                 .and("mainboard_cnt").is(dto.getMainboard_cnt())
                 .and("vga_cnt").is(dto.getVga_cnt())
                 .and("use").is(dto.getUse())
-                .and("program").is(dto.getProgram())
                 .and("price").gte(dto.getLowPrice()).lte(dto.getHighPrice());
 
+        // 프로그램은 선택사항
+        if(dto.getProgram() != null && dto.getProgram().length() != 0) {
+            defaultCriteria.and("program").is(dto.getProgram());
+        }
+
         /*
-          선택한부품이
+          선택한부품 조건
          */
         Map<String, Integer> products = dto.getProds();
         for(String key : products.keySet()) {
@@ -53,12 +71,13 @@ public class RecommendServiceImpl implements RecommendService {
 
         Query defaultQuery = Query.query(defaultCriteria).with(Sort.by(Sort.Direction.DESC, "cnt"));
 
-
         List<RecommendBuilderDto> result = mongoTemplate.find(
                 defaultQuery,
                 RecommendBuilderDto.class,
                 "builder"
         );
+
+        // 최대 10개로 제한
         if(result.size() > 10) {
             result = new ArrayList<>(result.subList(0, 10));
         }
@@ -74,8 +93,6 @@ public class RecommendServiceImpl implements RecommendService {
             for(int i=0; i<9; i++) {
                 prodDetail[i] = new HashMap<>();
             }
-
-
 
             String prods[] = r.getId().split(",");
             for(String prod : prods) {
@@ -97,8 +114,10 @@ public class RecommendServiceImpl implements RecommendService {
                     prod_idx = prod.substring(startIdx, endIdx);
                 }
 
+                String prodName = prod_idx + "_" + this.getProductNameByIdx(Long.parseLong(prod_idx), category);
+
                 // 같은 키는 없다.
-                prodDetail[category-1].put(prod_idx, prod_cnt);
+                prodDetail[category-1].put(prodName, prod_cnt);
             }
             response.add(RecommendBuilderResponseDto.builder()
                     .prodDetail(prodDetail)
@@ -110,5 +129,55 @@ public class RecommendServiceImpl implements RecommendService {
 
     public int countChar(String str, char ch) {
         return str.length() - str.replace(String.valueOf(ch), "").length();
+    }
+
+    public String getProductNameByIdx(long idx, int productCategory) {
+        Map<String, String> ret = new HashMap<>();
+        if (productCategory == 1) {
+            Cpu cpu = cpuRepository.findByIdx(idx).orElseThrow(
+                    () -> new CommentApiException(CommentErrorCode.PRODUCT_NOT_FOUND)
+            );
+            return cpu.getName();
+        } else if (productCategory == 2) {
+            Ram product = ramRepository.findByIdx(idx).orElseThrow(
+                    () -> new CommentApiException(CommentErrorCode.PRODUCT_NOT_FOUND)
+            );
+            return product.getName();
+        } else if (productCategory == 3) {
+            Hdd product = hddRepository.findByIdx(idx).orElseThrow(
+                    () -> new CommentApiException(CommentErrorCode.PRODUCT_NOT_FOUND)
+            );
+            return product.getName();
+        } else if (productCategory == 4) {
+            Ssd product = ssdRepository.findByIdx(idx).orElseThrow(
+                    () -> new CommentApiException(CommentErrorCode.PRODUCT_NOT_FOUND)
+            );
+            return product.getName();
+        } else if (productCategory == 5) {
+            Psu product = psuRepository.findByIdx(idx).orElseThrow(
+                    () -> new CommentApiException(CommentErrorCode.PRODUCT_NOT_FOUND)
+            );
+            return product.getName();
+        } else if (productCategory == 6) {
+            Cooler product = coolerRepository.findByIdx(idx).orElseThrow(
+                    () -> new CommentApiException(CommentErrorCode.PRODUCT_NOT_FOUND)
+            );
+            return product.getName();
+        } else if (productCategory == 7) {
+            Cases product = casesRepository.findByIdx(idx).orElseThrow(
+                    () -> new CommentApiException(CommentErrorCode.PRODUCT_NOT_FOUND)
+            );
+            return product.getName();
+        } else if (productCategory == 8) {
+            MainBoard product = mainBoardRepository.findByIdx(idx).orElseThrow(
+                    () -> new CommentApiException(CommentErrorCode.PRODUCT_NOT_FOUND)
+            );
+            return product.getName();
+        } else {
+            Vga product = vgaRepository.findByIdx(idx).orElseThrow(
+                    () -> new CommentApiException(CommentErrorCode.PRODUCT_NOT_FOUND)
+            );
+            return product.getName();
+        }
     }
 }
