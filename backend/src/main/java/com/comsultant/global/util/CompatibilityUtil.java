27ce3bdf0446d2e7d84739b4e09bc5740f8fcba9
yoojt233 <yoojt233@naver.com>
2022-10-05 13:11:45 +0900

    feat: 호환성 검사 기능 구현

--- backend/src/main/java/com/comsultant/global/util/CompatibilityUtil.java
+++ backend/src/main/java/com/comsultant/global/util/CompatibilityUtil.java
@@ -75,7 +75,10 @@ public class CompatibilityUtil {
                     Cooler tmp = coolerRepository.findByIdx(pIdx).orElseThrow(
                             () -> new ProductApiException(ProductErrorCode.PRODUCT_NOT_FOUND)
                     );
-                    if(tmp.getType())
+
+                    // Cpu Cooler만
+                    if("CPU쿨러".equals(tmp.getType()))
+                        cooler = tmp;
                     break;
                 case 7:
                     cases = casesRepository.findByIdx(pIdx).orElseThrow(
@@ -104,8 +107,44 @@ public class CompatibilityUtil {
 
         // mainboard<->cases 크기(ATX) 비교
         switch (mb.getFormFactor()){
-
-
+            case "ATX":
+                if(!cases.isStandardAtx())
+                    return false;
+                break;
+            case "M-ATX":
+                if(!cases.isMicroAtx())
+                    return false;
+                break;
+            case "E-ATX":
+                if(!cases.isExtendedAtx())
+                    return false;
+                break;
+            case "CEB":
+                if(!cases.isSsiCeb())
+                    return false;
+                break;
+            case "EEB":
+                if(!cases.isSsiEeb())
+                    return false;
+                break;
+            case "M-ITX":
+                if(!cases.isMiniItx())
+                    return false;
+                break;
+            case "M-DTX":
+                if(!cases.isMiniDtx())
+                    return false;
+                break;
+            case "XL-ATX":
+                if(!cases.isExtendedAtx())
+                    return false;
+                String pciSlot = cases.getPciSlot();
+                boolean flag = (pciSlot.contains("8개") || pciSlot.contains("9개") || pciSlot.contains("10개") || pciSlot.contains("12개"));
+                if(!flag)
+                    return false;
+                break;
+            default:
+                return false;
         }
 
         // mainboard<->ram 규격 개수 비교
@@ -120,14 +159,65 @@ public class CompatibilityUtil {
         if(vga != null && cases.getGpuMounting() < vga.getWidth())
             return false;
 
-        // cases<->psu 깊이 비교
+        // cases<->psu 깊이 크기 비교
         if(cases.getPowerMounting() < psu.getDepth())
             return false;
+        int pSize = sep(cases.getPowerSize());
+        switch (psu.getType()){
+            case "ATX 파워":
+                if(pSize < 3)
+                    return false;
+                break;
+            case "M-ATX(SFX) 파워":
+                if(pSize < 2)
+                    return false;
+                break;
+            case "Flex-ATX 파워":
+            case "TFX 파워":
+                if(pSize != 100)
+                    return false;
+                break;
+            case "DC to DC":
+                if(pSize < 1)
+                    return false;
+                break;
+            case "서버용 파워":
+                if(!"랙마운트".equals(cases.getClassType()))
+                    return false;
+                break;
+            case "리던던트":
+                if(pSize != 200)
+                    return false;
+            default:
+                break;
+        }
 
         // cases<->cooler 높이 비교
         if(cooler != null && cases.getGpuMounting() < cooler.getCoolerHeight())
             return false;
 
         return true;
+    }
+
+    private int sep(String powerSize) {
+        switch (powerSize){
+            case "ATX 파워" :
+            case "표준-ATX (M-ATX 지원)":
+            case "표준-ATX, SFX-L":
+            case "표준-ATX, Mini-redundant":
+                return 3;
+            case "M-ATX(SFX) 파워":
+                return 2;
+            case "DC to DC":
+                return 1;
+            case "Flex-ATX 파워":
+            case "FLEX, 1U SMPS":
+            case "TFX 파워":
+                return 100;
+            case "리던던트":
+                return 200;
+            default:
+                return 0;
+        }
     }
 }
