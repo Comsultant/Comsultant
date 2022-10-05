b5c661b014114cfdcd71d454e7de1fc4a78d8fff
yoojt233 <yoojt233@naver.com>
2022-10-05 16:56:03 +0900

    fix: 부품이 없는 경우 해당 부품의 호환성 검사를 하지 않도록 변경

--- backend/src/main/java/com/comsultant/global/util/CompatibilityUtil.java
+++ backend/src/main/java/com/comsultant/global/util/CompatibilityUtil.java
@@ -103,101 +103,113 @@ public class CompatibilityUtil {
 
         // 호환성 검사
         // cpu<->mainboard 제조사 소켓 비교
-        if(!cpu.getSocket().equals(mb.getCpuSocket()))
-            return "Error : cpu and mainboard";
+        if(cpu != null && mb != null) {
+            if (!cpu.getSocket().equals(mb.getCpuSocket()))
+                return "Error : cpu and mainboard";
+        }
 
         // mainboard<->cases 크기(ATX) 비교
-        switch (mb.getFormFactor()){
-            case "ATX":
-                if(!cases.isStandardAtx())
-                    return "Error : ATX Mainboard";
-                break;
-            case "M-ATX":
-                if(!cases.isMicroAtx())
-                    return "Error : M-ATX Mainboard";
-                break;
-            case "E-ATX":
-                if(!cases.isExtendedAtx())
-                    return "Error : E-ATX Mainboard";
-                break;
-            case "CEB":
-                if(!cases.isSsiCeb())
-                    return "Error : CEB Mainboard";
-                break;
-            case "EEB":
-                if(!cases.isSsiEeb())
-                    return "Error : EEB Mainboard";
-                break;
-            case "M-ITX":
-                if(!cases.isMiniItx())
-                    return "Error : M-ITX Mainboard";
-                break;
-            case "M-DTX":
-                if(!cases.isMiniDtx())
-                    return "Error : M-DTX Mainboard";
-                break;
-            case "XL-ATX":
-                if(!cases.isExtendedAtx())
-                    return "Error : XL-ATX Mainboard";
-                String pciSlot = cases.getPciSlot();
-                boolean flag = (pciSlot.contains("8개") || pciSlot.contains("9개") || pciSlot.contains("10개") || pciSlot.contains("12개"));
-                if(!flag)
-                    return "Error : XL-ATX Mainboard Not Enough Slot";
-                break;
-            default:
-                return "Error : Unknown mainboard size";
+        if(mb != null && cases != null) {
+            switch (mb.getFormFactor()) {
+                case "ATX":
+                    if (!cases.isStandardAtx())
+                        return "Error : ATX Mainboard";
+                    break;
+                case "M-ATX":
+                    if (!cases.isMicroAtx())
+                        return "Error : M-ATX Mainboard";
+                    break;
+                case "E-ATX":
+                    if (!cases.isExtendedAtx())
+                        return "Error : E-ATX Mainboard";
+                    break;
+                case "CEB":
+                    if (!cases.isSsiCeb())
+                        return "Error : CEB Mainboard";
+                    break;
+                case "EEB":
+                    if (!cases.isSsiEeb())
+                        return "Error : EEB Mainboard";
+                    break;
+                case "M-ITX":
+                    if (!cases.isMiniItx())
+                        return "Error : M-ITX Mainboard";
+                    break;
+                case "M-DTX":
+                    if (!cases.isMiniDtx())
+                        return "Error : M-DTX Mainboard";
+                    break;
+                case "XL-ATX":
+                    if (!cases.isExtendedAtx())
+                        return "Error : XL-ATX Mainboard";
+                    String pciSlot = cases.getPciSlot();
+                    boolean flag = (pciSlot.contains("8개") || pciSlot.contains("9개") || pciSlot.contains("10개") || pciSlot.contains("12개"));
+                    if (!flag)
+                        return "Error : XL-ATX Mainboard Not Enough Slot";
+                    break;
+                default:
+                    return "Error : Unknown mainboard size";
+            }
         }
 
         // mainboard<->ram 규격 개수 비교
-        int cnt = 0;
-        for(Ram ram : ramList){
-            cnt += ram.getLamCnt();
-            if(!mb.getMemoryType().equals(ram.getType()) || cnt > mb.getMemorySlot())
-                return "Error : mainboard and ram";
+        if(mb != null && !ramList.isEmpty()) {
+            int cnt = 0;
+            for (Ram ram : ramList) {
+                cnt += ram.getLamCnt();
+                if (!mb.getMemoryType().equals(ram.getType()) || cnt > mb.getMemorySlot())
+                    return "Error : mainboard and ram";
+            }
         }
 
         // cases<->vga 가로 비교
-        if(vga != null && cases.getGpuMounting() < vga.getWidth())
-            return "Error : case and vga";
+        if(cases != null && vga != null) {
+            if (cases.getGpuMounting() < vga.getWidth())
+                return "Error : case and vga";
+        }
 
-        // cases<->psu 깊이 크기 비교
-        if(cases.getPowerMounting() != 0 && cases.getPowerMounting() < psu.getDepth())
-            return "Error : case and psu";
-        int pSize = sep(cases.getPowerSize());
-        switch (psu.getType()){
-            case "ATX 파워":
-                if(pSize < 3)
-                    return "Error : ATX Power";
-                break;
-            case "M-ATX(SFX) 파워":
-                if(pSize < 2)
-                    return "Error : SFX Power";
-                break;
-            case "Flex-ATX 파워":
-            case "TFX 파워":
-                if(pSize != 100)
-                    return "Error : TFX, FATX Power";
-                break;
-            case "DC to DC":
-                if(pSize < 1)
-                    return "Error : DC Power";
-                break;
-            case "서버용 파워":
-                if(!"랙마운트".equals(cases.getClassType()))
-                    return "Error : Server Power";
-                break;
-            case "리던던트":
-                if(pSize != 200)
-                    return "Error : Redundant Power";
-            default:
-                break;
+        if(cases != null && psu != null) {
+            // cases<->psu 깊이 크기 비교
+            if (cases.getPowerMounting() != 0 && cases.getPowerMounting() < psu.getDepth())
+                return "Error : case and psu";
+            int pSize = sep(cases.getPowerSize());
+            switch (psu.getType()) {
+                case "ATX 파워":
+                    if (pSize < 3)
+                        return "Error : ATX Power";
+                    break;
+                case "M-ATX(SFX) 파워":
+                    if (pSize < 2)
+                        return "Error : SFX Power";
+                    break;
+                case "Flex-ATX 파워":
+                case "TFX 파워":
+                    if (pSize != 100)
+                        return "Error : TFX, FATX Power";
+                    break;
+                case "DC to DC":
+                    if (pSize < 1)
+                        return "Error : DC Power";
+                    break;
+                case "서버용 파워":
+                    if (!"랙마운트".equals(cases.getClassType()))
+                        return "Error : Server Power";
+                    break;
+                case "리던던트":
+                    if (pSize != 200)
+                        return "Error : Redundant Power";
+                default:
+                    break;
+            }
         }
 
         // cases<->cooler 높이 비교
-        if(cooler != null && "공랭".equals(cooler.getCoolingSystem()) && cases.getGpuMounting() < cooler.getCoolerHeight())
-            return "Error : case and cooler";
-        else if(cooler != null && "수랭".equals(cooler.getCoolingSystem()))
-            return "수랭 쿨러는 미완..."
+        if(cases != null && cooler != null) {
+            if (cooler != null && "공랭".equals(cooler.getCoolingSystem()) && cases.getGpuMounting() < cooler.getCoolerHeight())
+                return "Error : case and cooler";
+            else if (cooler != null && "수랭".equals(cooler.getCoolingSystem()))
+                return "수랭 쿨러는 미완...";
+        }
         return "success";
     }
 
