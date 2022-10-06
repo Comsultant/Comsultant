bcb70617d2737167b5eb2159f7817453e72a4c07
김진회 <kku64r@gmail.com>
2022-10-06 14:20:15 +0900

    feat: 내 찜목록 조회 및 내 댓글 조회 수정

--- backend/src/main/java/com/comsultant/domain/wish/service/WishServiceImpl.java
+++ backend/src/main/java/com/comsultant/domain/wish/service/WishServiceImpl.java
@@ -2,12 +2,14 @@ package com.comsultant.domain.wish.service;
 
 import com.comsultant.domain.account.entity.Account;
 import com.comsultant.domain.product.entity.Product;
-import com.comsultant.domain.product.repository.ProductRepository;
+import com.comsultant.domain.product.repository.*;
+import com.comsultant.domain.product.service.ProductServiceImpl;
 import com.comsultant.domain.wish.dto.WishDto;
 import com.comsultant.domain.wish.dto.WishListDto;
 import com.comsultant.domain.wish.entity.Wish;
 import com.comsultant.domain.wish.mapper.WishMapper;
 import com.comsultant.domain.wish.repository.WishRepository;
+import com.comsultant.global.config.security.AccountDetails;
 import com.comsultant.global.error.exception.ProductApiException;
 import com.comsultant.global.error.model.ProductErrorCode;
 import com.comsultant.global.properties.ConstProperties;
@@ -30,6 +32,16 @@ public class WishServiceImpl implements WishService {
     private final WishRepository wishRepository;
     private final ProductRepository productRepository;
     private final ConstProperties constProperties;
+    private final CpuRepository cpuRepository;
+    private final RamRepository ramRepository;
+    private final SsdRepository ssdRepository;
+    private final HddRepository hddRepository;
+    private final MainBoardRepository mainBoardRepository;
+    private final CoolerRepository coolerRepository;
+    private final CasesRepository casesRepository;
+    private final VgaRepository vgaRepository;
+    private final PsuRepository psuRepository;
+    private final ProductServiceImpl productServiceImpl;
 
     @Override
     public boolean createLike(Account account, long productIdx) {
@@ -59,7 +71,7 @@ public class WishServiceImpl implements WishService {
     }
 
     @Override
-    public WishListDto getLikes(Account account, int page, boolean desc) {
+    public WishListDto getLikes(Account account, int page, boolean desc, AccountDetails accountDetails) {
         if(account == null || account.getIdx() == 0) {
             return null;
         }
@@ -76,12 +88,46 @@ public class WishServiceImpl implements WishService {
 
         List<WishDto> list = new ArrayList<>();
 
-        for (Wish wish : wishes)
-            list.add(WishMapper.mapper.toDto(wish));
+        for (Wish wish : wishes) {
+            WishDto wishDto = WishMapper.mapper.toDto(wish);
+            Product product = null;
+            product = wish.getProduct();
+            if(product != null) {
+                int category = product.getCategory();
+                wishDto.inputCategory(category);
+                String name = findName(wish.getProduct().getIdx(), category, accountDetails);
+                wishDto.inputProductName(name);
+            }
+            list.add(wishDto);
+        }
 
         return WishListDto.builder()
                 .wishDtoList(list)
                 .totalPage(totalPages)
                 .build();
+    }
+
+    @Override
+    public String findName(long idx, int type, AccountDetails accountDetails) {
+        if (type == 1) { // 1.cpu, 2.ram, 3.hdd, 4.ssd, 5.psu, 6.cooler, 7.cases, 8.mainboard, 9.vga
+            return productServiceImpl.getCpu(idx, accountDetails).getName();
+        } else if (type == 2) {
+            return productServiceImpl.getRam(idx, accountDetails).getName();
+        } else if (type == 3) {
+            return productServiceImpl.getHdd(idx, accountDetails).getName();
+        } else if (type == 4) {
+            return productServiceImpl.getSsd(idx, accountDetails).getName();
+        } else if (type == 5) {
+            return productServiceImpl.getPsu(idx, accountDetails).getName();
+        } else if (type == 6) {
+            return productServiceImpl.getCooler(idx, accountDetails).getName();
+        } else if (type == 7) {
+            return productServiceImpl.getCases(idx, accountDetails).getName();
+        } else if (type == 8) {
+            return productServiceImpl.getMainBoard(idx, accountDetails).getName();
+        } else if (type == 9) {
+            return productServiceImpl.getVga(idx, accountDetails).getName();
+        }
+        return null;
     }
 }
